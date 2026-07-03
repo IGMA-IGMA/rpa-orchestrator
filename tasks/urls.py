98d27d0764f8a19b6d8159ab5d0384ecdfc19062
IGMA-IGMA <i9296517650@yandex.ru>
2026-07-03 14:45:39 +0300

    feat: add URL routing for tasks app

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
-# Пока просто заглушка
+app_name = 'tasks'
+
 urlpatterns = [
-    # Здесь позже будут пути
+    path('', views.TaskListView.as_view(), name='task_list'),
+    path('create/', views.TaskCreateView.as_view(), name='task_create'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('export/', views.export_csv, name='export_csv'),
+    path('health/', views.health_check, name='health_check'),
 ]
